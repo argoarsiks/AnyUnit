f0a199dffcfe0d0cdd9c3397e90a369255863e99
argoarsiks <smetankauwu3008@gmail.com>
2025-05-23 18:40:08 +0300

    Add getAllUnits

--- anyunit/__init__.py
+++ anyunit/__init__.py
@@ -1,2 +1,3 @@
 from .converter import convert_length, convert_weight, convert_area
-__all__ = ['convert_length', 'convert_weight', 'convert_area']
+from .getUnits import getAllUnits
+__all__ = ['convert_length', 'convert_weight', 'convert_area', 'getAllUnits']
